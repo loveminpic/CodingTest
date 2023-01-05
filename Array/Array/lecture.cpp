7046c4f1eddb3234d67106993e6039eb6aa5c664
Minji Lee <43211858+loveminpic@users.noreply.github.com>
2023-01-05 16:35:08 +0900

    made erase function

--- Array/Array/lecture.cpp
+++ Array/Array/lecture.cpp
@@ -30,52 +30,54 @@ void insert(int idx, int num, int arr[], int& len){
     len = len + 1;
 }
 
-//void erase(int idx, int arr[], int& len){
-//
-//}
+void erase(int idx, int arr[], int& len){
+    
+    int temp1;
+    
+    if(len == idx){
+        arr[idx] = 0;
+        len = len - 1;
+        return ;
+    }
+    
+    for(int i = idx; i < len ; i++){
+        temp1 = arr[i+1];
+        arr[i] = temp1;
+    }
+    len = len - 1;
+
+}
 
 void printArr(int arr[], int& len){
   for(int i = 0; i < len; i++) cout << arr[i] << ' ';
   cout << "\n\n";
 }
 
-int main(void) {
-    
-    cout << "***** insert_test *****\n";
-      int arr[10] = {10, 20, 30};
-      int len = 3;
-      insert(3, 40, arr, len); // 10 20 30 40
-      printArr(arr, len);
-      insert(1, 50, arr, len); // 10 50 20 30 40
-      printArr(arr, len);
-      insert(0, 15, arr, len); // 15 10 50 20 30 40
-      printArr(arr, len);
+void insert_test(){
+  cout << "***** insert_test *****\n";
+  int arr[10] = {10, 20, 30};
+  int len = 3;
+  insert(3, 40, arr, len); // 10 20 30 40
+  printArr(arr, len);
+  insert(1, 50, arr, len); // 10 50 20 30 40
+  printArr(arr, len);
+  insert(0, 15, arr, len); // 15 10 50 20 30 40
+  printArr(arr, len);
 }
-//void insert_test(){
-//  cout << "***** insert_test *****\n";
-//  int arr[10] = {10, 20, 30};
-//  int len = 3;
-//  insert(3, 40, arr, len); // 10 20 30 40
-//  printArr(arr, len);
-//  insert(1, 50, arr, len); // 10 50 20 30 40
-//  printArr(arr, len);
-//  insert(0, 15, arr, len); // 15 10 50 20 30 40
-//  printArr(arr, len);
-//}
-//
-//void erase_test(){
-//  cout << "***** erase_test *****\n";
-//  int arr[10] = {10, 50, 40, 30, 70, 20};
-//  int len = 6;
-//  erase(4, arr, len); // 10 50 40 30 20
-//  printArr(arr, len);
-//  erase(1, arr, len); // 10 40 30 20
-//  printArr(arr, len);
-//  erase(3, arr, len); // 10 40 30
-//  printArr(arr, len);
-//}
-//
-//int main(void) {
-//  insert_test();
-//  erase_test();
-//}
+
+void erase_test(){
+  cout << "***** erase_test *****\n";
+  int arr[10] = {10, 50, 40, 30, 70, 20};
+  int len = 6;
+  erase(4, arr, len); // 10 50 40 30 20
+  printArr(arr, len);
+  erase(1, arr, len); // 10 40 30 20
+  printArr(arr, len);
+  erase(3, arr, len); // 10 40 30
+  printArr(arr, len);
+}
+
+int main(void) {
+  insert_test();
+  erase_test();
+}
